f617d9559322cbb8f2d1ec80fb8995fc179e3faf
jichanghir <serkinworld@gmail.com>
2019-05-25 11:10:38 +0300

    fix db connection in server

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,3 +1,11 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@db/codegig`)
+const db = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@db/codegig`);
+
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
