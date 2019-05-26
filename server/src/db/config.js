57145d86c8dd03eebde6265e8f610f84d66738dc
jichanghir <serkinworld@gmail.com>
2019-05-26 22:21:12 +0300

    update .sequalizerc

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -8,8 +8,12 @@ module.exports = {
     use_env_variable: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@db/${process.env.DB_NAME}`
   },
   test: {
-    dialect: "postgres",
-    storage: ":memory:"
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: 'db',
+    dialect: 'postgres',
+    use_env_variable: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@db/${process.env.DB_NAME}`
   },
   production: {
     username: process.env.DB_USER,
@@ -18,5 +22,7 @@ module.exports = {
     host: 'db',
     dialect: 'postgres',
     use_env_variable: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@db/${process.env.DB_NAME}`
-  }
+  },
+  "migrations-path": path.join(__dirname, 'migrations'),
+  "models-path": path.join(__dirname, 'models'),
 };
